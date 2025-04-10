4eb7c006ab1745ca809839dd9534b4878c175a53
Tou Fong Lee <leetoufong@gmail.com>
2025-04-10 12:30:14 -0400

    More UI updates

--- src/App.tsx
+++ src/App.tsx
@@ -27,22 +27,26 @@ function App() {
     }
 
     return (
-        <div className="App">
-            <h1 className="text-5xl font-bold uppercase text-yellow-400">Random Chuck Norris Joke Generator</h1>
+        <div className="text-center p-20 w-full max-w-[900px] ml-auto mr-auto">
+            <h1 className="font-bold uppercase text-yellow-300 mb-10"><span className="block text-6xl">Random</span> <span className="block text-8xl">Chuck Norris</span> <span className="block text-6xl">Joke Generator</span></h1>
 
-            <button className="bg-gray-900 rounded border-gray-500 px-2 py-1" onClick={() => getJoke('random')}>Generate Joke</button>
+            <div className="mb-5">
+                <button className="bg-gray-900 rounded border-gray-500 px-2 py-1" onClick={() => getJoke('random')}>Generate a joke</button>
+            </div>
 
-            <label htmlFor="categories">Or chose a joke from category</label>
-            <select id="categories" className="text-gray-900 rounded border px-2 py-1" onChange={(event) => getJoke(`random?category=${event.target.value}`)}>
-                <option>Select a category</option>
-                <>
-                    {categories.map((category, index) => {
-                        return <option key={index} value={category}>{category.charAt(0).toUpperCase() + category.slice(1)}</option>
-                    })}
-                </>
-            </select>
+            <div className="mb-10">
+                <label className="mr-3" htmlFor="categories">Or chose a joke from category:</label>
+                <select id="categories" className="text-gray-900 rounded border px-2 py-1" onChange={(event) => getJoke(`random?category=${event.target.value}`)}>
+                    <option>Select a category</option>
+                    <>
+                        {categories.map((category, index) => {
+                            return <option key={index} value={category}>{category.charAt(0).toUpperCase() + category.slice(1)}</option>
+                        })}
+                    </>
+                </select>
+            </div>
 
-            <p>{joke}</p>
+            <p className="text-yellow-300 text-4xl">{joke}</p>
         </div>
     )
 }
