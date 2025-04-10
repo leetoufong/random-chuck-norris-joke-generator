c4ec4688b6c7250842b83b435d8af37745fe92d4
Tou Fong Lee <leetoufong@gmail.com>
2025-04-10 12:42:40 -0400

    Adjust yellow text color

--- src/App.tsx
+++ src/App.tsx
@@ -28,14 +28,14 @@ function App() {
 
     return (
         <div className="text-center p-20 w-full max-w-[900px] ml-auto mr-auto">
-            <h1 className="font-bold uppercase text-yellow-300 mb-10"><span className="block text-6xl">Random</span> <span className="block text-8xl">Chuck Norris</span> <span className="block text-6xl">Joke Generator</span></h1>
+            <h1 className="font-bold uppercase text-yellow-400 mb-10"><span className="block text-6xl">Random</span> <span className="block text-8xl">Chuck Norris</span> <span className="block text-6xl">Joke Generator</span></h1>
 
             <div className="mb-5">
                 <button className="bg-gray-900 rounded border-gray-500 px-2 py-1" onClick={() => getJoke('random')}>Generate a joke</button>
             </div>
 
             <div className="mb-10">
-                <label className="mr-3" htmlFor="categories">Or chose a joke from category:</label>
+                <label className="mr-3" htmlFor="categories">Or chose a category:</label>
                 <select id="categories" className="text-gray-900 rounded border px-2 py-1" onChange={(event) => getJoke(`random?category=${event.target.value}`)}>
                     <option>Select a category</option>
                     <>
@@ -46,7 +46,7 @@ function App() {
                 </select>
             </div>
 
-            <p className="text-yellow-300 text-4xl">{joke}</p>
+            <p className="text-yellow-400 text-4xl">{joke}</p>
         </div>
     )
 }
